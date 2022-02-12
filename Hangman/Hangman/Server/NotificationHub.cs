0c87cfe443607de7f7cf9f037fe92b81903b08b8
Hunter Olson <hunterolson@mail.weber.edu>
2022-02-11 17:18:00 -0700

    Tried to salt/hash

--- Hangman/Hangman/Server/NotificationHub.cs
+++ Hangman/Hangman/Server/NotificationHub.cs
@@ -4,9 +4,9 @@ namespace Hangman.Server
 {
     public class NotificationHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string hashedSaltedPass, string salt)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, hashedSaltedPass, salt);
         }
     }
 }
