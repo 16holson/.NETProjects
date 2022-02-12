19e1892ff9f64e158ba11187e9a39e1c5773cdf0
Tage Higley <tagehigley@mail.weber.edu>
2022-02-12 01:30:37 -0700

    added image functionality

---
+++ Hangman/Hangman/Shared/Assets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Shared {
+    public class Assets {
+
+        public string? AssetFile { get; set; }
+
+        public Assets(string? assetFile) {
+            this.AssetFile = assetFile;
+        }
+    }
+}
