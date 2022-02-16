ad0e54ccd12a6268b026e33fa03a079d821da98b
Cecilia Harvey <Cecilia.Darley@gmail.com>
2022-02-15 18:28:51 -0700

    Added DatabaseConnector class and User class

---
+++ Hangman/Hangman/Server/CustomClasses/DatabaseConnector.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Odbc;
+
+namespace Hangman.Server.CustomClasses
+{
+    public class DatabaseConnector
+    {
+		static void ProcessFile(string folderPath)
+		{
+			//the file pattern is *output.accdb
+			var file = Directory.GetFiles(@folderPath, "*output.accdb").FirstOrDefault();
+			if (File.Exists(file))
+			{
+				string connectionstring = null;
+				connectionstring = @"Driver={Microsoft Access Driver 
+						  (*.mdb, *.accdb)};DBQ=" + file;
+				OdbcConnection odbcConnection = new OdbcConnection(connectionstring);
+				try
+				{
+					odbcConnection.Open();
+					List<string> tableNames = new List<string>();
+					var schema = odbcConnection.GetSchema("Tables");
+
+					foreach (System.Data.DataRow row in schema.Rows)
+					{
+						var tableName = row["TABLE_NAME"].ToString();
+						//Exclude the system tables
+						if (!tableName.StartsWith("MSys"))
+						{
+							tableNames.Add(tableName);
+						}
+					}
+
+					foreach (var tableName in tableNames)
+					{
+						Console.WriteLine(tableName);
+					}
+					odbcConnection.Close();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+		}
+	}
+}
---
+++ Hangman/Hangman/Server/CustomClasses/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Hangman.Server.CustomClasses
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }   
+        public string Salt { get; set; }
+        public int HighScore { get; set; }  
+
+        public User(string userName, string passwordHash, string salt)
+        {
+            this.UserName = userName;   
+            this.PasswordHash = passwordHash;
+            this.Salt = salt;
+        }
+    }
+}
