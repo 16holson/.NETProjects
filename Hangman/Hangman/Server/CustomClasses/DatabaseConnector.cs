7b3cc875d11f2866b1a76e2d8fdcb315e4f71708
Cecilia Harvey <celia@ceciliaharvey.info>
2022-02-20 13:36:32 -0700

    Updated InsertUser() to work with variables, instead of hardcoded

--- Hangman/Hangman/Server/CustomClasses/DatabaseConnector.cs
+++ Hangman/Hangman/Server/CustomClasses/DatabaseConnector.cs
@@ -4,12 +4,32 @@ namespace Hangman.Server.CustomClasses
 {
     public class DatabaseConnector
     {
+		/// <summary>
+		/// String that gives all data necessary to connect to access db file
+		/// </summary>
 		public string ConnectionString { get; set; }
-		public string FolderPath { get; set; }
-		public string FileName { get; set; }
-		public string TableName { get; set; } = "Users";
-		public int NumRowsAffected { get; set; } = 0;
 
+		/// <summary>
+		/// Filepath to get to the folder containing the .accdb file
+		/// </summary>
+		public string FolderPath { get; set; }
+		
+		/// <summary>
+		/// Name of the .accdb file
+		/// </summary>
+		public string FileName { get; set; }
+		
+		/// <summary>
+		/// Name of the table you want to search or alter
+		/// </summary>
+		public string TableName { get; set; } = "Users";
+
+		/// <summary>
+		/// Constructor for DatabaseConnector class
+		/// Sets the FolderPath, FileName and ConnectionString
+		/// </summary>
+		/// <param name="filepath"></param>
+		/// <param name="filename"></param>
 		public DatabaseConnector(string filepath, string filename)
         {
 			FolderPath = filepath;	
@@ -22,38 +42,20 @@ namespace Hangman.Server.CustomClasses
 			}
 		}
 
-		public void Connect()
-        {
-			try
-			{
-				var odbcConnection = new OdbcConnection(ConnectionString);
-				odbcConnection.Open();
-			}
-			catch (Exception ex)
-            {
-				throw new Exception(ex.Message);
-            }
-        }
-
-		public void CloseDB()
-		{
-			try
-			{
-				OdbcConnection odbcConnection = new OdbcConnection(ConnectionString);
-				odbcConnection.Close();
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(ex.Message);
-			}
-		}
-
-		public int InsertUser()
+		/// <summary>
+		/// Inserts a user into the Users table in the HangmanDB.accdb file
+		/// </summary>
+		/// <param name="userName"></param>
+		/// <param name="hashedPass"></param>
+		/// <param name="salt"></param>
+		/// <returns></returns>
+		/// <exception cref="Exception"></exception>
+		public int InsertUser(string userName, string hashedPass, string salt)
         {
 			using (OdbcConnection odbcConnection = new OdbcConnection(ConnectionString))
 			{
 				OdbcCommand command = odbcConnection.CreateCommand();
-				command.CommandText = "INSERT INTO Users (username, hash, salt) VALUES ('useruseruser', 'passhashsalt', '12345')";
+				command.CommandText = "INSERT INTO Users (username, hash, salt) VALUES ('" + userName + "', '" + hashedPass + "', '" + salt + "')";
 				try
 				{
 					odbcConnection.Open();
@@ -66,6 +68,12 @@ namespace Hangman.Server.CustomClasses
 			}
         }
 
+		/// <summary>
+		/// Example function from the tutorial I was following
+		/// returns a list of the rows in the .accdb file
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="Exception"></exception>
 		public static List<string> ProcessFile()
 		{
 			string folderPath = "./assets/";
@@ -93,10 +101,10 @@ namespace Hangman.Server.CustomClasses
 						}
 					}
 
-					// foreach (var tableName in tableNames)
-					// {
-					// 	Console.WriteLine(tableName);
-					// }
+					foreach (var tableName in tableNames)
+					{
+						Console.WriteLine(tableName);
+					}
 					odbcConnection.Close();
 					return tableNames;
 				}
--- Hangman/UnitTests/DatabaseConnectorTest.cs
+++ Hangman/UnitTests/DatabaseConnectorTest.cs
@@ -19,29 +19,18 @@ namespace UnitTests
             Assert.IsInstanceOfType(dbConnector, typeof(DatabaseConnector));
         }
 
-        // [TestMethod]
-        // public void TestOpenConnection()
-        // {
-        //     // Arrange
-        //     string filepath = "../../../../Hangman/Server/assets";
-        //     string filename = "HangmanDB.accdb";
-        //     DatabaseConnector dbConnector = new DatabaseConnector(filepath, filename);
-        //     // Try
-        //     dbConnector.Connect();
-        //     // Assert
-        //     // Not sure what to assert. Connect() didn't throw an exception, though. 
-        //     dbConnector.CloseDB();  
-        // 
-        // }
-
         [TestMethod]
         public void TestInsertUser()
         {
             string filepath = "../../../../Hangman/Server/assets";
             string filename = "HangmanDB.accdb";
             DatabaseConnector dbConnector = new DatabaseConnector(filepath, filename);
+            byte[] salt = SaltyHash.GenerateSalt();
+            byte[] hashedPass = SaltyHash.ComputeSha256Hash(SaltyHash.ConvertToBytes("password"), salt);
+            string saltString = SaltyHash.ConvertToString(salt);
+            string hashedPassString = SaltyHash.ConvertToString(hashedPass);
             // Try
-            Assert.AreEqual(1, dbConnector.InsertUser());
+            Assert.AreEqual(1, dbConnector.InsertUser("testingtester" + saltString, hashedPassString, saltString));
         }
     }
 }
